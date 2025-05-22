a35e520e4553e6a23dff369b1817e535d3999786
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-05-22 09:26:00 +0200

    Register IronEnchantment

--- src/main/kotlin/wdfeer/iron_refabricated/IronEnchantment.kt
+++ src/main/kotlin/wdfeer/iron_refabricated/IronEnchantment.kt
@@ -4,11 +4,20 @@ import net.minecraft.enchantment.Enchantment
 import net.minecraft.enchantment.EnchantmentTarget
 import net.minecraft.enchantment.Enchantments
 import net.minecraft.entity.EquipmentSlot
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+import wdfeer.iron_refabricated.IronRefabricated.MOD_ID
+
 
 object IronEnchantment : Enchantment(Rarity.RARE, EnchantmentTarget.WEAPON, arrayOf(EquipmentSlot.MAINHAND)) {
     // TODO: implement iron nugget drops on kill
 
     override fun canAccept(other: Enchantment?): Boolean {
         return super.canAccept(other) && other != Enchantments.LOOTING
+    }
+
+    fun register() {
+        Registry.register(Registries.ENCHANTMENT, Identifier.of(MOD_ID, "iron_enchantment"), this)
     }
 }
--- src/main/kotlin/wdfeer/iron_refabricated/IronRefabricated.kt
+++ src/main/kotlin/wdfeer/iron_refabricated/IronRefabricated.kt
@@ -4,9 +4,11 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object IronRefabricated : ModInitializer {
+	const val MOD_ID = "iron_refabricated"
     private val logger = LoggerFactory.getLogger("iron_refabricated")
 
 	override fun onInitialize() {
-		logger.info("Hello Fabric world!")
+		IronEnchantment.register()
+		logger.info("Iron just got refabricated!")
 	}
 }
