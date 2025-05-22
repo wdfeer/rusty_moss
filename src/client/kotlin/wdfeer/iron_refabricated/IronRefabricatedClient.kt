497e318e8cc860aed10ef48dc4a6339bd646af34
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-05-22 12:58:59 +0200

    Create models, blockstates

--- src/client/kotlin/wdfeer/iron_refabricated/IronRefabricatedClient.kt
+++
@@ -1,9 +0,0 @@
-package wdfeer.iron_refabricated
-
-import net.fabricmc.api.ClientModInitializer
-
-object IronRefabricatedClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
